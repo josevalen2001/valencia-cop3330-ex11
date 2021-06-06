53c60b93a9f4643a2ae6cc51165aa0b53f86d0bb
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-06 19:09:08 -0400

    Final Commit

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -1,4 +1,5 @@
 package base;
+import java.text.DecimalFormat;
 import java.util.*;
 
 /*
@@ -10,8 +11,21 @@ public class app {
 
     public static void main(String[] args) {
         Scanner newScan = new Scanner(System.in);
+        DecimalFormat numFormat = new DecimalFormat("#.00");
 
-        
+        //Input
+        System.out.print("How many euros are you exchanging? ");
+        double euros = newScan.nextDouble();
+
+        System.out.print("What is the exchange rate? ");
+        double rate = newScan.nextDouble();
+
+        //Calculations
+        double dollars = euros * rate;
+
+        //Output
+        System.out.println(euros + " at an exchange rate of " + rate + " is");
+        System.out.println(numFormat.format(dollars) + " U.S. dollars.");
     }
 
 }
